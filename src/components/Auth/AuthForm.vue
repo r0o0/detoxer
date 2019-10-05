360312d21ab87825d355c97f1a0af50c2baa8cfe
r0o0 <rayeoh7@gmail.com>
2019-10-05 11:50:27 +0900

    add user signup form: email password auth

---
+++ src/components/Auth/AuthForm.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  el-dialog(
+    title="회원가입"
+    :visible.sync="showDialog"
+  )
+    div.email-auth
+      el-form
+        el-form-item(label="이메일")
+          el-input(placeholder="gildong@detoxer.com")
+        el-form-item(label="비밀번호" prop="pass")
+          el-input(type="password" autocomplete="off")
+        el-form-item
+          el-button(type="primary") 회원가입
+          el-button(plain) 취소
+      span 이미 회원이신가요? 로그인
+    FirebaseAuth
+</template>
+
+<script>
+import FirebaseAuth from './FirebaseAuth'
+
+export default {
+  name: 'AuthForm',
+  data () {
+    return {
+      showDialog: true
+    }
+  },
+  components: {
+    FirebaseAuth
+  }
+}
+</script>
