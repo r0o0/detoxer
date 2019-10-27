ddfe9d055565ca186ffac6ab113e687ab8724db0
r0o0 <rayeoh7@gmail.com>
2019-10-27 18:47:14 +0900

    add document.cookies utils

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,14 +1,15 @@
 // 쿠키 헬퍼 함수 모음
 
 export function getCookie (name) {
+  let result = null
   const cookies = document.cookie.split(';')
   cookies.forEach((cookie, i) => {
     const splitCookie = cookie.split('=')
     const key = splitCookie[0].trim()
     const value = splitCookie[1]
-    if (key === name) return value
-    return null
+    if (key === name) result = value
   })
+  return result
 }
 
 export function setCookie (name, value, maxDays) {
@@ -24,4 +25,8 @@ export function setCookie (name, value, maxDays) {
 export function isCookieExist (name) {
   if (getCookie(name)) return true
   return false
+}
+
+export function deleteCookie (name) {
+  document.cookie = `${name}=; path=/; max-age=0`
 }
